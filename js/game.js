93515e86a6f408385ca9a53928782516bcfd06a4
omriex <omarshafee037@gmail.com>
2026-07-31 21:29:24 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@
             try {
                 let res = await fetch('https://raw.githubusercontent.com/omriex/vertopia-io/main/index.html?t=' + Date.now(), { cache: 'no-store' });
                 let text = await res.text();
-                let match = text.match(/<div class="version-text">(v[\d\.]+)<\/div>/);
+                let match = text.match(/<div class="version-text">(v[\d\.]+)<\/div>/i); 
                 if (match && match[1] !== CURRENT_VERSION) {
                     window.location.reload(true);
                 }
